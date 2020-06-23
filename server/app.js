f02ae250529105e0594af67e0db1934090677b1b
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-06-22 21:58:58 -0700

    Initial Upload. Passes all tests.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+const cache = {};
+
+app.use(morgan('dev'));
+
+// When making calls to the OMDB API make sure to append the '&apikey=8730e0ez' d786d4d5 parameter
+app.get('/', (req, res) => {
+    const url = req.originalUrl;
+    
+    if(cache[url]) {
+        res.json(cache[url]);
+    } else {
+        axios.get(`http://www.omdbapi.com${url}&apikey=8730e0e`)
+            .then(({data}) => {
+                res.json(data);
+                cache[url] = data;
+            })
+            .catch(err => res.status(400).json(err));
+    }
+});
+
 
 module.exports = app;
